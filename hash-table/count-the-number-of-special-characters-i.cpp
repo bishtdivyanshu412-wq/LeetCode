72492dfd32383d9a5c8df9306036ea6f506e2e4c
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-05-27 15:27:22 +0530

    LC-count-the-number-of-special-characters-i [Runtime: N/A; Memory: N/A]

--- hash-table/count-the-number-of-special-characters-i.cpp
+++ hash-table/count-the-number-of-special-characters-i.cpp
@@ -3,18 +3,18 @@ public:
     int numberOfSpecialChars(string words) {
      int n = words.size();
      vector<int>ans(26,0);
-     vector<int>res(26,0);
+     int count =0;
      for(int i =0;i<n;i++){
         if(words[i]>='a' &&  words[i] <= 'z'){
            ans[words[i]-'a']++;
          }
         else if(words[i]>='A' && words[i]<='Z'){
-            res[words[i]-'A']++;
+            char ch = tolower(words[i]);
+            if(ans[ch-'a']!=0){
+                count++;
+            }
+              ans[ch-'a'] = INT_MIN;
         }
-     }
-     int count = 0;
-     for(int i = 0 ; i<res.size();i++){
-        if(res[i]!=0 && ans[i]!=0) count++;
      }
      return count;
     }
