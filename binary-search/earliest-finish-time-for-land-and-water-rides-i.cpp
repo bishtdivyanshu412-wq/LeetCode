475243b67d665aa2acabd6e3295a89d2f8f6b3ec
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-02 11:12:26 +0530

    LC-earliest-finish-time-for-land-and-water-rides-i [Runtime: N/A; Memory: N/A]

--- binary-search/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ binary-search/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-    int n = landStartTime.size();
-    
-    int x = waterStartTime.size();
-     int cost = INT_MAX; 
-    for(int i =0;i<n;i++){
-        for(int j =0;j<x;j++){
-            if(landStartTime[i] + landDuration[i] >= waterStartTime[j]){
-              int cost1 = landStartTime[i] + landDuration[i] + waterDuration[j];
-              cost = min(cost , cost1);
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
+
+        int ans = INT_MAX;
+
+        // Land -> Water
+        for (int i = 0; i < n; i++) {
+            int landEnd = landStartTime[i] + landDuration[i];
+
+            for (int j = 0; j < m; j++) {
+                int finish =
+                    max(landEnd, waterStartTime[j]) + waterDuration[j];
+
+                ans = min(ans, finish);
             }
         }
-    }
-    for(int i  = 0;i<x;i++){
-        for(int k =0;k<n;k++){
-            if(waterStartTime[i] + waterDuration[i] >= landStartTime[k]){
-                int cost1 = waterStartTime[i] + waterDuration[i] + landDuration[k];
-                cost = min(cost,cost1);
+
+        // Water -> Land
+        for (int i = 0; i < m; i++) {
+            int waterEnd = waterStartTime[i] + waterDuration[i];
+
+            for (int j = 0; j < n; j++) {
+                int finish =
+                    max(waterEnd, landStartTime[j]) + landDuration[j];
+
+                ans = min(ans, finish);
             }
         }
-    }
-    return cost;
+
+        return ans;
     }
 };
