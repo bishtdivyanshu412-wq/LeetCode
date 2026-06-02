62cb7689e627a5b8748f3a7a7d7c9aae4054e35a
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-02 12:45:13 +0530

    LC-increasing-triplet-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/increasing-triplet-subsequence.cpp
+++ greedy/increasing-triplet-subsequence.cpp
@@ -3,7 +3,7 @@ public:
     bool increasingTriplet(vector<int>& nums) {
       int n = nums.size();
       for(int i =0;i<n-2;i++){
-        if(nums[i]<nums[i+1]<nums[i+2]){
+        if(nums[i]<nums[i+1] && nums[i+1]<nums[i+2]){
             return true;
         }
       }
