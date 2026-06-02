c687c335647128fbfab9f9695580f580e02f6578
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-02 11:32:00 +0530

    LC-minimum-cost-of-buying-candies-with-discount [Runtime: N/A; Memory: N/A]

--- greedy/minimum-cost-of-buying-candies-with-discount.cpp
+++ greedy/minimum-cost-of-buying-candies-with-discount.cpp
@@ -3,6 +3,9 @@ public:
     int minimumCost(vector<int>& cost) {
      sort(cost.begin(),cost.end());
      int n = cost.size();
+     if(n==1){
+        return 0;
+     }
      int totalcost = 0;
      for(int i = n-1;i>0;i-=3){
       if((i-1)>=0){
