45d62560e55a94ab89090fbdd65e796da164cf03
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-07 23:09:25 +0530

    LC-middle-of-the-linked-list [Runtime: 0 ms; Memory: 10.1 MB]

--- two-pointers/middle-of-the-linked-list.cpp
+++ two-pointers/middle-of-the-linked-list.cpp
@@ -11,30 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-     ListNode* temp = head ;
-     int count = 0;
-     while(temp!=NULL){
-        count++;
-        temp = temp->next;
+     ListNode* slow = head ;
+     ListNode* fast = head;
+     
+     while(fast!=NULL && fast->next!= NULL){
+        slow = slow->next;
+        fast = fast->next->next;
      } 
-     ListNode* curr = head;  
-     if(count%2 !=0){
-        int n = 1;
-     while(curr!=NULL){
-        if(n == (count+1)/2) return curr;
-        n++;
-        curr = curr->next;
-     }
-     }
-     else {
-        int n = 1;
-        ListNode*head1 = head;
-        while(head1!=NULL){
-            if(n == (count/2)+1) return head1;
-            n++;
-            head1 = head1->next;
-        }
-     }
-     return head;
+     
+     return slow;
     }
 };
