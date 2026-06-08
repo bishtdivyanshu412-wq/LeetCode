a0499b98d0fde83898ed98317fb2631ccc55f91b
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-08 09:59:21 +0530

    LC-partition-array-according-to-given-pivot [Runtime: N/A; Memory: N/A]

--- two-pointers/partition-array-according-to-given-pivot.cpp
+++ two-pointers/partition-array-according-to-given-pivot.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> left;
-        vector<int> middle;
+       int count = 0;
         vector<int>right;
         int n = nums.size();
         for(int i =0;i<n;i++){
             if(nums[i]>pivot) right.push_back(nums[i]);
             else if(nums[i]<pivot) left.push_back(nums[i]);
-            else if(nums[i] == pivot ) middle.push_back(nums[i]);
+            else if(nums[i] == pivot ) count++;
         }
         for(int i =0;i<left.size();i++){
          nums[i] = left[i];   
         }
         int j = left.size();
-        for(int i = 0;i<middle.size();i++){
-            nums[j] = middle[i];
+        for(int i = count ;i>0;i--){
+            nums[j] = pivot;
             j++;
         }
         for(int i = 0;i<right.size();i++){
