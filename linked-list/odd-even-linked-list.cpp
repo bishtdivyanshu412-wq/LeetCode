34d4f4e3ae68d63c978ad936c90b08832ea283c9
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-08 16:55:58 +0530

    LC-odd-even-linked-list [Runtime: N/A; Memory: N/A]

--- linked-list/odd-even-linked-list.cpp
+++ linked-list/odd-even-linked-list.cpp
@@ -11,11 +11,12 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head ;
      ListNode* odd = head;
      ListNode* even = head->next;
      ListNode* evenhead = head->next;
 
-     while(even != NULL && even->next!=NULL){
+     while( even != NULL && even->next!=NULL){
         odd->next = odd->next->next;
         odd = odd->next;
 
