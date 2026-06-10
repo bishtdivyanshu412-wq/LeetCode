2b67bf363fd9b354485d07b16e5c828c9ca306f5
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-10 20:59:22 +0530

    LC-sort-even-and-odd-indices-independently [Runtime: 0 ms; Memory: 15.8 MB]

---
+++ sorting/sort-even-and-odd-indices-independently.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+     int n = nums.size();
+     vector<int> odd ;
+     vector<int> even;
+     for(int i =0;i<n;i++){
+        if(i%2 == 0) even.push_back(nums[i]);
+        else odd.push_back(nums[i]);
+     }
+
+     sort(odd.begin(),odd.end(),greater<int>());
+     sort(even.begin(),even.end());
+    
+
+     int j =0;
+     int k =0;
+
+     for(int i =0;i<n;i++){
+        if(i%2 == 0) {
+            nums[i] = even[j];
+            j++;
+        }
+        else{
+            nums[i] = odd[k];
+            k++;
+        }
+     }
+      return nums;
+    }
+};
