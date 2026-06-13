f57e485d022075240ef4c732b61f9b5b4ede970f
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-13 22:47:26 +0530

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

---
+++ linked-list/linked-list-cycle-ii.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+      if(head == NULL || head->next == NULL) return NULL;
+    ListNode* temp = head ;
+    while(temp->val != INT_MIN && temp->next != NULL){
+     temp->val = INT_MIN;
+     temp = temp->next;
+    }   
+    if(temp->val == INT_MIN) return temp;
+    return NULL;  
+    }
+};
