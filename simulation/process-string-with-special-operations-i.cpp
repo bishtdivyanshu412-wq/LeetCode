5d104fa0697a22f5563c8705f12f6648dfa7e8c8
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-16 09:54:16 +0530

    LC-process-string-with-special-operations-i [Runtime: N/A; Memory: N/A]

---
+++ simulation/process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+       string result;
+       int n = s.size();
+       for(int i =0;i<n;i++){
+        int sz = result.size();
+        if(s[i] == '*' && !result.empty()) result.pop_back();
+        else if(s[i] == '#') {
+         for(int j =0;j<sz;j++){
+            result.push_back(result[j]);
+         }
+        }
+        else if(s[i] == '%') reverse(result.begin(),result.end());
+        else result.push_back(s[i]);
+       } 
+       return result;
+    }
+};
