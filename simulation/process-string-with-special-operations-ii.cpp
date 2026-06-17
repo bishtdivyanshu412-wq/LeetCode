6e61229706990d926e2985762936dadc0ae066f5
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-17 21:58:43 +0530

    LC-process-string-with-special-operations-ii [Runtime: N/A; Memory: N/A]

--- simulation/process-string-with-special-operations-ii.cpp
+++ simulation/process-string-with-special-operations-ii.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
     char processStr(string s, long long k) {
-       string result;
-       int n = s.size();
-       for(int i =0;i<n;i++){
-        int sz = result.size();
-        if(s[i] == '*' && !result.empty()) result.pop_back();
-        else if(s[i] == '#') {
-         for(int j =0;j<sz;j++){
-            result.push_back(result[j]);
-         }
+        int n = s.size();
+
+        vector<long long> len(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+
+            if (ch >= 'a' && ch <= 'z') {
+                len[i + 1] = min(k + 1, len[i] + 1);
+            }
+            else if (ch == '*') {
+                len[i + 1] = max(0LL, len[i] - 1);
+            }
+            else if (ch == '#') {
+                len[i + 1] = min(k + 1, len[i] * 2);
+            }
+            else if (ch == '%') {
+                len[i + 1] = len[i];
+            }
         }
-        else if(s[i] == '%') reverse(result.begin(),result.end());
-        else if(s[i]>='a' && s[i]<='z')result.push_back(s[i]);
-       } 
-       
-       if(k==0 && result.size() == 0) return '.';
-       else if(k<=result.size()) return result[k];
-       return '.';
+
+        if (k >= len[n]) return '.';
+
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = s[i];
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (k == len[i]) return ch;
+            }
+            else if (ch == '*') {
+                if (len[i] > len[i + 1]) {
+                    
+                    if (k == len[i + 1]) return '.';
+                }
+            }
+            else if (ch == '#') {
+                long long oldLen = len[i];
+                if (k >= oldLen) k -= oldLen;
+            }
+            else if (ch == '%') {
+                k = len[i] - 1 - k;
+            }
+        }
+
+        return '.';
     }
 };
