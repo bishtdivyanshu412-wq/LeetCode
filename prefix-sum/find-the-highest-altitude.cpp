8f2e604f00e1064e72a4f80e65030126145c2b2d
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-19 10:11:53 +0530

    LC-find-the-highest-altitude [Runtime: N/A; Memory: N/A]

--- prefix-sum/find-the-highest-altitude.cpp
+++ prefix-sum/find-the-highest-altitude.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-     int n = gain.size();
-     int alt = 0;
-     int maxalt =0;
-     for(int i =0;i<n;i++){
-      alt = alt + gain[i];
-      maxalt = max(alt,maxalt);  
-     }   
-     return maxalt;
+    int altitude = 0;
+    int maxalt = 0; 
+    int n = gain.size();
+
+    for(int i =0;i<n;i++){
+        int alt = gain[i] + altitude;
+        altitude = alt;
+        maxalt = max(altitude,maxalt);
+    }    
+    return maxalt;
     }
 };
