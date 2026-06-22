c4ccfd1e5293900b1e9059f32307d045b61211f2
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-06-22 11:50:06 +0530

    LC-maximum-number-of-balloons [Runtime: N/A; Memory: N/A]

---
+++ hash-table/maximum-number-of-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string s) {
+     string target = "ballon"; 
+
+     int count = 0;
+
+      while(true){
+     for(int i =0;i<target.size();i++){
+        int count1 =0;
+        for(int j =0;j<s.size();j++){
+            if(target[i] == s[j]) {
+                 s[j] = '#';
+                 count1++;
+                 break;
+            }
+        }
+        if(count1 == 0) return count;
+     }
+      count++;
+     } 
+     return 0;  
+    }
+  
+}; 
+  
