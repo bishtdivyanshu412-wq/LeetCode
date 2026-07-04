61619ff4e8b927046efd5e9ceb115b13617a0492
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-04 13:37:35 +0530

    LC-combination-sum-ii [Runtime: N/A; Memory: N/A]

--- backtracking/combination-sum-ii.cpp
+++ backtracking/combination-sum-ii.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
+    void solve(int idx, vector<int>& candidates, vector<int>& ds,
+               vector<vector<int>>& result, int target) {
 
-
-    void solve(int idx , vector<int>&candidates , vector<vector<int>>& result , vector<int>ds, int target){
-        if(idx == candidates.size()){
-            if(target == 0){
-                result.push_back(ds);
-            }
-            return; 
+        if (target == 0) {
+            result.push_back(ds);
+            return;
         }
-        
-        if(candidates[idx]<=target){
-        ds.push_back(candidates[idx]);
 
-            solve(idx+1,candidates,result,ds,target-candidates[idx]);
+        for (int i = idx; i < candidates.size(); i++) {
+
             
+            if (i > idx && candidates[i] == candidates[i - 1])
+                continue;
 
+            
+            if (candidates[i] > target)
+                break;
+
+            ds.push_back(candidates[i]);
+            solve(i + 1, candidates, ds, result, target - candidates[i]);
             ds.pop_back();
-
         }
-        solve(idx+1,candidates,result,ds,target);
     }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        
-        int n = candidates.size();
-       
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>>result;
-        vector<int>ds;
 
-        solve(0,candidates,result,ds,target);
-        set<vector<int>> st;
+        sort(candidates.begin(), candidates.end());
 
-        for(int i =0;i<result.size();i++){
-            st.insert(result[i]);
-        }
+        vector<vector<int>> result;
+        vector<int> ds;
 
-        vector<vector<int>> ans;
+        solve(0, candidates, ds, result, target);
 
-        for(auto x : st){
-            ans.push_back(x);
-        }
-        return ans;
+        return result;
     }
 };
