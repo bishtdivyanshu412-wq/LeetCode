34e25184da99c2aca762c8c5195a8843c522cd4f
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-07 20:43:48 +0530

    LC-power-of-three [Runtime: N/A; Memory: N/A]

--- math/power-of-three.cpp
+++ math/power-of-three.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-    if(n==0 || n==-1) return false;
+    if(n<=0) return false;
     if(n==1 ) return true;
     if(n==2) return false;
 
