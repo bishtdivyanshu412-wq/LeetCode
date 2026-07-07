af97cf59d75f447d8a90e90a091ccefce4597d7c
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-07 20:50:46 +0530

    LC-power-of-four [Runtime: N/A; Memory: N/A]

--- recursion/power-of-four.cpp
+++ recursion/power-of-four.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n<=0) return false;
-        if(n==1) return true;
+        int count = 0;
+        vector<int> ans;
 
-        if(n==2 || n==3) return false;
-
-        while(n>1){
-            if(n%4 != 0) return false;
-            n = n/4;
+        while(n!=0){
+            int rem = n&1;
+            ans.push_back(rem);
+            n = n>>1;
         }
-        return true;
+
+        for(int i =0;i<ans.size();i++){
+            if(ans[i] == 1) count++;
+        }
+
+        if(count == 1) return true;
+        return false;
     }
 };
