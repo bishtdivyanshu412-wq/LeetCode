13a42e5edea4d5541f60d7e2f80fff35eb5c0288
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-07 14:46:23 +0530

    LC-minimum-bit-flips-to-convert-number [Runtime: N/A; Memory: N/A]

--- bit-manipulation/minimum-bit-flips-to-convert-number.cpp
+++ bit-manipulation/minimum-bit-flips-to-convert-number.cpp
@@ -1,47 +1,13 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-      int s = start;
-      int g = goal;  
-
       int count = 0;
+      int xr = start^goal;
 
-      vector<int>start1;
-      vector<int>goal1;
-
-
-      while(s!=0){
-        int bit = s&1;
-        start1.push_back(bit);
-        s = s>>1;
-      }
-      while(g!=0){
-        int bit = g&1;
-        goal1.push_back(bit);
-        g = g>>1;
-      }
-
-      int i =0;
-      int j =0;
-
-      while(i<start1.size()&& j<goal1.size()){
-        if(goal1[j] != start1[i]){
-            count++;
-        }  
-        i++;
-        j++;      
-      }
-      while(j<goal1.size()){
-        if(goal1[j] == 1){
-        count++;
-        }
-        j++;
-      }
-      while(i<start1.size()){
-        if(start1[i] == 1){
-        count++;
-        }
-        i++;
+      while(xr!=0){
+        int bit = xr&1;
+        if(bit == 1) count++;
+        xr = xr>>1;
       }
       return count;
     }
