60239f03f0ff39a76bb5d64d4861c9c746436dac
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-08 09:21:44 +0530

    LC-find-the-xor-of-numbers-which-appear-twice [Runtime: N/A; Memory: N/A]

--- hash-table/find-the-xor-of-numbers-which-appear-twice.cpp
+++ hash-table/find-the-xor-of-numbers-which-appear-twice.cpp
@@ -3,13 +3,13 @@ public:
     int duplicateNumbersXOR(vector<int>& nums) {
      int n = nums.size();
 
-     vector<int>result(50,0);
+     vector<int>result(51,0);
 
      for(int i =0;i<n;i++){
         result[nums[i]]++;
      } 
       int xr = 0;
-     for(int i =0;i<n;i++){
+     for(int i =1;i<result.size();i++){
         if(result[i] == 2) xr  = xr^i;
      }  
      return xr;
