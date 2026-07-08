57301c5c62ff9c40395b249b8e16a3afcc8d4b53
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-08 08:58:58 +0530

    LC-single-number-ii [Runtime: N/A; Memory: N/A]

--- array/single-number-ii.cpp
+++ array/single-number-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
        int n = nums.size(); 
+       if(n==1) return nums[0];
        sort(nums.begin(),nums.end());
        if(nums[0]!=nums[1]) return nums[0];
        if(nums[n-1]!=nums[n-2]) return nums[n-1];
