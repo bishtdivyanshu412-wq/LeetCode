7fcb458a4b3b7129ba8f8045c23de4b754054cd2
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-08 20:52:01 +0530

    LC-subsets [Runtime: N/A; Memory: N/A]

--- array/subsets.cpp
+++ array/subsets.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-
-    void print(int idx , vector<int>&ds , vector<vector<int>>& sub , vector<int>& nums){
-        if(idx == nums.size()){
-            sub.push_back(ds);
-            return;
-        }
-
-        ds.push_back(nums[idx]);
-        print(idx+1,ds,sub,nums);
-
-        ds.pop_back();
-        print(idx+1,ds,sub,nums);
-
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-      vector<vector<int>> sub;
-      vector<int>ds;
-      print(0,ds,sub,nums); 
-      return sub; 
+      int n = nums.size();
+
+      vector<vector<int>>result;
+
+      int subset = 1<<n;
+
+      for(int i = 0;i<subset;i++){
+        vector<int>ans;
+
+        for(int  j =0;j<n;j++){
+            if(i&(1<<j)) ans.push_back(nums[j]);
+        }
+        result.push_back(ans);
+      }  
+      return result;
     }
 };
