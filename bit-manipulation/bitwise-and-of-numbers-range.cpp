b97ba798a38f6fb289a3dcdafabcfeb1c8a0565f
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-09 12:23:26 +0530

    LC-bitwise-and-of-numbers-range [Runtime: N/A; Memory: N/A]

--- bit-manipulation/bitwise-and-of-numbers-range.cpp
+++ bit-manipulation/bitwise-and-of-numbers-range.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
+        if(right - left == 1) return 0;
         int num = left;
         for(int i = left+1; i<right;i++){
             num = num&i;
