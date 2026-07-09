3e0c2caab9d01dbb798ffdd960cb26f1b0386cbc
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-09 11:16:01 +0530

    LC-counting-bits [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/counting-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+       vector<int>result;
+
+       for(int i =0;i<=n;i++){
+        int count = 0;
+        int num = i;
+
+        while(num != 0){
+            int bit = num&1;
+            if(bit == 1) count++;
+            num = num>>1;
+        }
+
+        result.push_back(count);
+        
+       } 
+
+       return result;
+    }
+};
