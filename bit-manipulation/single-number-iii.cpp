ae0c8a1bf80746c04970e0fa50fa12c82b0432ff
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-10 11:56:42 +0530

    LC-single-number-iii [Runtime: N/A; Memory: N/A]

--- bit-manipulation/single-number-iii.cpp
+++ bit-manipulation/single-number-iii.cpp
@@ -3,17 +3,19 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         int xr = 0;
 
-       
-        for (int num : nums)
+        for (int num : nums) {
             xr ^= num;
+        }
 
-       
-        int rightMost = xr & (-xr);
+        
+        unsigned int rightMost =
+            static_cast<unsigned int>(xr) &
+            -static_cast<unsigned int>(xr);
 
         int a = 0, b = 0;
 
         for (int num : nums) {
-            if (num & rightMost)
+            if (static_cast<unsigned int>(num) & rightMost)
                 a ^= num;
             else
                 b ^= num;
