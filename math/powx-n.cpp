38f1c5baa4757ebbb2bad454a4fa6ce7692b9b67
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-11 11:20:18 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.cpp
+++ math/powx-n.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-      return pow(x,n);  
+      double ans = 1;
+
+      for(int i = 0 ;i<n;i++){
+        ans = ans*x;
+      } 
+      return ans;
     }
 };
