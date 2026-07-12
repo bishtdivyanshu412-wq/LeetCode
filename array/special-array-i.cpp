05cd710fb1a45a842e8e7b05290bbdd7abbc7b21
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-12 14:38:11 +0530

    LC-special-array-i [Runtime: N/A; Memory: N/A]

---
+++ array/special-array-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+     int n = nums.size();
+
+     if(nums[0]%2 == 0){
+        for(int i =0;i<n;i = i+2){
+            if(nums[i]%2 != 0) return false;
+        }
+     }
+     else{
+        for(int i =0;i<n;i =i+2){
+            if(nums[i]%2 == 0) return false;
+        }
+     } 
+     return true;  
+    }
+};
