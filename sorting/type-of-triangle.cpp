5ced3acb3904ff179e31e931812ed9a3acc3ca45
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-12 14:33:59 +0530

    LC-type-of-triangle [Runtime: N/A; Memory: N/A]

--- sorting/type-of-triangle.cpp
+++ sorting/type-of-triangle.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-     if (nums[2]>=nums[0]+nums[1]) return "none";
-if (nums[0]==nums[2]) return "equilateral";
-if (nums[0]==nums[1] || nums[1]==nums[2]) return "isosceles";
-return "scalene"; 
+        set<int>s(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
+        if((nums[0] + nums[1])<=nums[2]) return "none";
+        if(s.size() == 1) return "equilateral";
+        if(s.size() == 2) return "isosceles";
+        return "scalene";
     }
 };
