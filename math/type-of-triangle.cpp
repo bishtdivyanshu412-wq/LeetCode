92c8f13c941efd1f1d9f1b9145d3319313eed794
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-12 14:33:27 +0530

    LC-type-of-triangle [Runtime: N/A; Memory: N/A]

--- math/type-of-triangle.cpp
+++ math/type-of-triangle.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-     int count1 = nums[0];
-     int count2 = nums[1];
-     int count3 = nums[2];
-
-     if(count1 == count2 && count2 == count3) return "equilateral";
-     else if(count1 == count2 && count2 != count3) return "isosceles";
-     else if (count1 == count3 && count2 != count1) return "isosceles";
-     else if (count1 != count2 && count2 == count3) return "isosceles";
-      return "scalene";   
+     if (nums[2]>=nums[0]+nums[1]) return "none";
+if (nums[0]==nums[2]) return "equilateral";
+if (nums[0]==nums[1] || nums[1]==nums[2]) return "isosceles";
+return "scalene"; 
     }
 };
