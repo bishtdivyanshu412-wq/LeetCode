a951fc85c16508cb4dd515da48be366ab478cdea
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-14 17:23:08 +0530

    LC-remove-duplicates-from-sorted-array-ii [Runtime: N/A; Memory: N/A]

---
+++ array/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+      int n = nums.size();
+      unordered_map<int,int> mpp;
+
+      for(int i =0;i<n;i++){
+        mpp[nums[i]]++;
+      } 
+      vector<int> ans;
+      int prev = -1;
+      for(int i =0;i<n;i++){
+        if(mpp[nums[i]] > 0 && prev != nums[i]){
+            if(mpp[nums[i]] == 1) ans.push_back(nums[i]);
+            else if(mpp[nums[i]] >=2) {
+                ans.push_back(nums[i]);
+                ans.push_back(nums[i]);
+            }
+        }
+
+        prev = nums[i];
+       
+      } 
+      
+
+      for(int k =0;k<ans.size();k++){
+        nums[k] = ans[k];
+      }
+
+      return ans.size();
+    }
+};
