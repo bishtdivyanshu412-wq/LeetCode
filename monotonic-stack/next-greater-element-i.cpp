9b27274a37fa7ba43fe49f73768f9e051fada9f0
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-17 20:10:04 +0530

    LC-next-greater-element-i [Runtime: N/A; Memory: N/A]

--- monotonic-stack/next-greater-element-i.cpp
+++ monotonic-stack/next-greater-element-i.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-     int n = nums1.size();   
-     vector<int>ans(n,-1);
+       int n = nums1.size();
+       int m = nums2.size();
 
-     
-     for(int i =0;i<n;i++){
-        int k = -1;
-        for(int j =0;j<nums2.size();j++){
-            if(nums1[i] == nums2[j]) k = j;
+       stack<int>st;
+       vector<int>ans(m,INT_MIN);
+
+       for (int i = m-1;i>=0;i--){
+
+        while(!st.empty() && nums2[i]>=st.top()){
+            st.pop();
         }
 
-        for(int z = k+1;z<nums2.size();z++){
-            if(nums1[i] < nums2[z]) {
-                ans[i] = nums2[z];
-                break;
-            }
+        if(st.empty()) {
+            ans[i] = -1;
+            st.push(nums2[i]);
         }
-     }
-        return ans;
+        else{
+            ans[i] = st.top();
+            st.push(nums2[i]);
+        }
+       }
+
+       for(int i =0;i<n;i++){
+        for(int j =0;j<m;j++){
+          if(nums1[i] == nums2[j]) {
+            nums1[i] = ans[j];
+            break;
+          }
+        }
+       }
+       return nums1;
     }
-
 };
