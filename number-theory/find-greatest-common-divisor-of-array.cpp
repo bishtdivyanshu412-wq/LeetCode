c5ff55b57093e8b755245664353847acaf5bd2bb
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-18 10:56:07 +0530

    LC-find-greatest-common-divisor-of-array [Runtime: N/A; Memory: N/A]

--- number-theory/find-greatest-common-divisor-of-array.cpp
+++ number-theory/find-greatest-common-divisor-of-array.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        
+    int n = nums.size();
+    int max = nums[0];
+    int min = nums[0];
+    int gcd =1; 
+    for(int i=0;i<n;i++){
+        if(nums[i]>max) max = nums[i];
+        if(nums[i]<min) min = nums[i];
+    } 
+    for(int i=1;i<max+1;i++){
+        if(min%i==0 && max%i==0){
+            gcd = i;
+        }
+    }  
+    return gcd;
     }
 };
