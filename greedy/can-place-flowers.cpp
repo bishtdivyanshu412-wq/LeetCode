3cd260bd71987638d4c8f74a5e978fc09c9cc6e3
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-19 22:37:40 +0530

    LC-can-place-flowers [Runtime: N/A; Memory: N/A]

--- greedy/can-place-flowers.cpp
+++ greedy/can-place-flowers.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& nums, int n) {
        int m = nums.size();
-       if(m == 1 && n==1) {
-        if(nums[0] == 0) return true;
-        else return false;
-       }
-       
-       if(nums[0] == 0 && nums[1] == 0) {
-        if(n>0) n--;
-        else n =0;
-        nums[0] = 1;
-
-       } 
-       if(nums[m-1] == 0 && nums[m-2] == 0) {
-        if(n>0) n--;
-        else n =0;
-        nums[m-1] = 1;
-       }
-        if(n==0) return true;
-       for(int i = 1 ; i< m-1;i++){
-        if(n== 0) return true;
-        if(nums[i] == 0 ){
-            if(nums[i-1] == 0 && nums[i+1] == 0) {
-                if(n>0) n--;
-                else n= 0;
-                nums[i] = 1;
+       if(m<=2) return false;
+        
+       int i =0;
+       while(i<m){
+        if(i == 0 && (nums[i]+nums[i+1]) == 0) {
+            n--;
+            i = i+2;
+        }
+        else if(nums[i] + nums[i+1] == 0 && nums[i] +  nums[i-1] == 0){
+            n--;
+            i = i+2;
+        }
+        else if(i == m-1){
+            if(nums[m-1]+nums[m-2] == 0){
+                n--;
             }
         }
+        i++;
+
+        if(n<=0) return true;
        }
-        return false;
+       return false;
     }
 };
