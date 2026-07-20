0f73b04c87be2273433cd34e9f50424f7cbef070
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-20 15:12:05 +0530

    LC-shift-2d-grid [Runtime: N/A; Memory: N/A]

--- array/shift-2d-grid.cpp
+++ array/shift-2d-grid.cpp
@@ -5,6 +5,39 @@ public:
      int n = grid[0].size();
 
      if(k % (n*m) == 0) return grid; 
+
+
+     if(k % n == 0){
+        int i =0;
+        int j = m-1;
+
+        while(i<j){
+            for(int k =0;k<n;k++){
+                swap(grid[i][k], grid[j][k]);
+            }
+            i++;
+        }
+        return grid;
+     }
+     else{
+        for(int i =1 ; i<=k;i++){
+            int j = 0;
+            int k = n-1;
+            while(j<k){
+                for(int z = 0;z<m;z++){
+                    swap(grid[z][j] , grid[z][k]);
+                }
+                j++;
+            }
+
+            int x = 0;
+            int y = m-1;
+            while(x<y){
+                swap(grid[x][0] , grid[y][0]);
+                x++;
+            }
+        }
+     }
      return grid;  
     }
 };
