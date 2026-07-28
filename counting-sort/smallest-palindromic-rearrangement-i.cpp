5d31f2bf8cad4e17cd4d74ee46576744e5f4ec99
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-28 07:12:06 +0530

    LC-smallest-palindromic-rearrangement-i [Runtime: N/A; Memory: N/A]

---
+++ counting-sort/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        if(n==1) return s;
+        vector<int>ans(26,0);
+
+        for(int i =0;i<n;i++){
+            ans[s[i]-'a']++;
+        }
+        
+        int k =0;
+        int j = n-1;
+
+        for(int i =0;i<ans.size();i++){
+            int count = ans[i];
+
+            while(count ){
+
+                if(count == 1){
+                    s[k] = 'a'+i;
+                    count = count  >> 1;
+                    k++;
+                }
+                s[k] = 'a'+i;
+                s[j] = 'a'+i;
+                k++;
+                j--;
+                count =  count >> 2;
+            }
+        }
+       
+       return s;
+    }
+};
