a2eb05a34ba1fa4ad1149bc2775236f619600e26
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-29 23:22:24 +0530

    LC-sum-of-subarray-minimums [Runtime: N/A; Memory: N/A]

--- stack/sum-of-subarray-minimums.cpp
+++ stack/sum-of-subarray-minimums.cpp
@@ -1,19 +1,59 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
+    vector<int> previousSmaller(vector<int>& arr) {
         int n = arr.size();
-        const int MOD = 1e9 + 7;
-
-        long long sum = 0;
+        vector<int> left(n);
+        stack<int> st;
 
         for (int i = 0; i < n; i++) {
-            long long mini = arr[i];
-            for (int j = i; j < n; j++) {
-                mini = min(mini, (long long)arr[j]);
-                sum = (sum + mini) % MOD;
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
             }
+
+            if (st.empty())
+                left[i] = i + 1;
+            else
+                left[i] = i - st.top();
+
+            st.push(i);
         }
 
-        return sum;
+        return left;
+    }
+
+    vector<int> nextSmaller(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> right(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                right[i] = n - i;
+            else
+                right[i] = st.top() - i;
+
+            st.push(i);
+        }
+
+        return right;
+    }
+
+    int sumSubarrayMins(vector<int>& arr) {
+        const int MOD = 1e9 + 7;
+
+        vector<int> left = previousSmaller(arr);
+        vector<int> right = nextSmaller(arr);
+
+        long long ans = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            ans = (ans + 1LL * arr[i] * left[i] * right[i]) % MOD;
+        }
+
+        return ans;
     }
 };
