a0196a36cecebd1d7d25317395814529b3c6c43e
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-31 00:14:48 +0530

    LC-find-the-distance-value-between-two-arrays [Runtime: N/A; Memory: N/A]

--- two-pointers/find-the-distance-value-between-two-arrays.cpp
+++ two-pointers/find-the-distance-value-between-two-arrays.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
+
+
+    bool bs(vector<int>& arr2 , int ans,int target){
+        int k = arr2.size();
+
+        int low = 0 ;
+        int high = k-1;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            if(abs(ans-arr2[mid])<=target) return true;
+            else if(abs(ans-arr2[mid])>target) high = mid-1;
+            else low = mid+1; 
+        } 
+        return false;
+    }
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
       int n = arr1.size();
       int m = arr2.size();
       int count = 0;
 
+      sort(arr2.begin(),arr2.end());
+
       for(int i =0;i<n;i++){
-        for(int j = 0;j<m;j++){
-            if(abs(arr1[i]-arr2[j]) <=d) {
-                count++;
-                break;
-            }
+        if(bs(arr2,arr1[i],d)){
+            count++;
         }
       }
-
       return (n-count);  
     }
 };
