42dc64fdbadee72aa6413d31ef19ba43618551f2
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-07-30 19:30:31 +0530

    LC-trapping-rain-water [Runtime: 0 ms; Memory: 26 MB]

--- array/trapping-rain-water.cpp
+++ array/trapping-rain-water.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-        int n = nums.size();
+       int n = nums.size();
 
-        int leftmax = 0 , rightmax =0 , total = 0;
+       int leftmax = 0;
+       int rightmax = 0;
 
-        int l = 0 , r = n-1;
-
-        while(l<r){
-            if(nums[l]<=nums[r]){
-                if(leftmax>nums[l]){
-                    total+=leftmax-nums[l];
-                    l = l+1;
-                }
-                else{
-                    leftmax = nums[l];
-                    l = l+1;
-                }
-             }
-             else{
-                if(rightmax>nums[r]){
-                total += rightmax - nums[r];
-                r = r-1;
-                }
-                else {
-                    rightmax = nums[r];
-                    r = r-1;
-             }
+       int i = 0;
+       int j = n-1;
+       int totalWater = 0;
+       while(i<=j){
+        if(leftmax<=rightmax){
+            if(nums[i]<leftmax){
+                totalWater += leftmax-nums[i];
+                i++;
+            }
+            else{
+                leftmax = nums[i];
+                i++;
             }
         }
-        return total;
+        else{
+            if(nums[j]<rightmax){
+                totalWater += rightmax-nums[j];
+                j--;
+            }
+            else{
+                rightmax = nums[j];
+                j--;
+            }
+        }
+       } 
+       return totalWater;
     }
 };
