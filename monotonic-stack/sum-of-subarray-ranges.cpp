65b41fea0802471172b2ef7dcf5a84917d221d1f
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-03 18:35:15 +0530

    LC-sum-of-subarray-ranges [Runtime: N/A; Memory: N/A]

--- monotonic-stack/sum-of-subarray-ranges.cpp
+++ monotonic-stack/sum-of-subarray-ranges.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
      int n = nums.size();
-     int sum =0;
+     long long sum =0;
      for(int i =0;i<n;i++){
         int maxi = nums[i];
         int mini = nums[i];
@@ -10,7 +10,7 @@ public:
           if(nums[j]>maxi) maxi = nums[j];
           if(nums[j]<mini) mini = nums[j];
 
-           sum += 1ll*(maxi - mini);
+           sum += (maxi - mini);
         }
      }   
      return sum;
