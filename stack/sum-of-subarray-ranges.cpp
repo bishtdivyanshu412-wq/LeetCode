21db3cc323b13804a6f0bcf81370cd9c4ebba12a
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-03 18:30:42 +0530

    LC-sum-of-subarray-ranges [Runtime: N/A; Memory: N/A]

---
+++ stack/sum-of-subarray-ranges.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+     int n = nums.size();
+     int sum =0;
+     for(int i =0;i<n;i++){
+        int maxi = nums[i];
+        int mini = nums[i];
+        for(int j = i;j<n;j++){
+          if(nums[j]>maxi) maxi = nums[j];
+          if(nums[j]<mini) mini = nums[j];
+
+           sum += maxi - mini;
+        }
+     }   
+     return sum;
+    }
+};
