22bd337d26551d30c38d3a65d5b63507f6d51852
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-06 15:07:54 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game.js
+++ dynamic-programming/jump-game.js
@@ -4,9 +4,13 @@
  */
 var canJump = function(nums) {
    let n  = nums.length;
-   if(n===1) return true;
-   for(let i =0;i<n;i++){
-    if(i + nums[i] === n-1) return true;
-   } 
+   let prev = nums[0];
+
+   for(let i = 0;i<n;i++){
+     prev = prev-1;
+    if(prev + i >= n-1) return true;
+    if(prev < nums[i]) prev = nums[i];
+    if(prev <0 && i != n-1) return false;
+   }
    return false;
 };
