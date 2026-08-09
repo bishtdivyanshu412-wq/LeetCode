8bd2727db0c4423943e635ff79bde5a55e51498c
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-09 22:04:51 +0530

    LC-jewels-and-stones [Runtime: N/A; Memory: N/A]

--- string/jewels-and-stones.cpp
+++ string/jewels-and-stones.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int numJewelsInStones(string js, string s) {
-        int n = js.size();
-        int m = s.size();
+        int freq[52] = {0};
+
+        for(char c : s) {
+            if(c >= 'a' && c <= 'z')
+                freq[c - 'a']++;
+            else
+                freq[c - 'A' + 26]++;
+        }
 
         int count = 0;
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(js[i] == s[j]) count++;
-            }
+
+        for(char c : js) {
+            if(c >= 'a' && c <= 'z')
+                count += freq[c - 'a'];
+            else
+                count += freq[c - 'A' + 26];
         }
+
         return count;
     }
 };
