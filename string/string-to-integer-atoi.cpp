3a036ec4714c610f176220b99288b67546563eeb
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-13 18:38:34 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     int myAtoi(string s) {
-      int n = s.size();
-      int ans = 0;
-      vector<int>result;
-      for(int i = 0;i<n;i++){
-        if(isdigit(s[i])) result.push_back(s[i]-'0');
-        else break;
-      }
-      for(int i =0;i<result.size();i++){
-        ans = ans*10 + result[i];
-      }
-      if(s[0] == '-') return -(ans);
-      return ans;
+        int n = s.size();
+        int ans = 0;
+        vector<int> result;
+
+        int i = 0;
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+        // Check sign
+        bool negative = false;
+
+        if (i < n && s[i] == '-') {
+            negative = true;
+            i++;
+        }
+        else if (i < n && s[i] == '+') {
+            i++;
+        }
+
+        // Collect digits
+        while (i < n && isdigit(s[i])) {
+            result.push_back(s[i] - '0');
+            i++;
+        }
+
+        // Build number
+        for (int j = 0; j < result.size(); j++) {
+            ans = ans * 10 + result[j];
+        }
+
+        // Apply sign
+        if (negative)
+            return -ans;
+
+        return ans;
     }
 };
