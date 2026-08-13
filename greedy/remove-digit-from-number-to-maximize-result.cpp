263bbb6a5d83096ecf33d95690324a0d63c3c10f
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-13 21:03:44 +0530

    LC-remove-digit-from-number-to-maximize-result [Runtime: N/A; Memory: N/A]

--- greedy/remove-digit-from-number-to-maximize-result.cpp
+++ greedy/remove-digit-from-number-to-maximize-result.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     string removeDigit(string number, char digit) {
-     int n = number.size();
-     int maxans = 0;
-     for(int i =0;i<n;i++){
-        if(number[i] == digit){
-            int ans = 0;
-            for(int j =0;j<n;j++){
-                if(j == i){
-                    continue;
+        int n = number.size();
+        string maxans = "";
+
+        for (int i = 0; i < n; i++) {
+            if (number[i] == digit) {
+                string ans = "";
+
+                for (int j = 0; j < n; j++) {
+                    if (j != i) {
+                        ans += number[j];
+                    }
                 }
-                else {
-                    ans = ans*10+ number[j]-'0';
+
+                if (ans > maxans) {
+                    maxans = ans;
                 }
             }
-            maxans = max(maxans,ans);
         }
-     } 
-     string s = to_string(maxans);
-     return s;  
+
+        return maxans;
     }
 };
