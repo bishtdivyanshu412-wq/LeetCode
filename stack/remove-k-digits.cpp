c58ec6179ee09d6ec03a547b5d16952df80f84f0
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-14 21:10:25 +0530

    LC-remove-k-digits [Runtime: N/A; Memory: N/A]

--- stack/remove-k-digits.cpp
+++ stack/remove-k-digits.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    string ans = "";
-
-    void solve(string &num, int i, int k, string curr) {
-        // processed all digits
-        if (i == num.size()) {
-            if (k == 0) {
-                // remove leading zeroes
-                int j = 0;
-                while (j < curr.size() && curr[j] == '0')
-                    j++;
-
-                string temp = curr.substr(j);
-
-                if (temp == "")
-                    temp = "0";
-
-                if (ans == "" || temp.size() < ans.size() ||
-                    (temp.size() == ans.size() && temp < ans)) {
-                    ans = temp;
-                }
-            }
-            return;
-        }
-
-        // Choice 1: keep current digit
-        solve(num, i + 1, k, curr + num[i]);
-
-        // Choice 2: remove current digit
-        if (k > 0) {
-            solve(num, i + 1, k - 1, curr);
-        }
-    }
-
     string removeKdigits(string num, int k) {
-        if (k == num.size())
-            return "0";
+     int n = num.size();
+     stack<char>st;
+     if(n==k) return "0";
+     string ans = "";
+     int i =0;
+    while(i<n){
+       
+        while(!st.empty() && st.top() > num[i] && k>0){
+            st.pop();
+            k--;
+        }
+        st.push(num[i]);
+        i++;
+     }  
+     while(!st.empty()) {
+    ans += st.top();
+    st.pop();
+}
+    reverse(ans.begin(), ans.end());
 
-        solve(num, 0, k, "");
+    int j = 0;
 
-        return ans;
+while(j < ans.size() && ans[j] == '0') {
+    j++;
+}
+
+ans = ans.substr(j);
+
+if(ans.empty())
+    return "0";
+
+return ans;
+     return ans;
     }
 };
