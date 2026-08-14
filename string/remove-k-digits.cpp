a5f68a8506b64c952d9dbaeb7ff47d0e926799dd
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-14 20:46:10 +0530

    LC-remove-k-digits [Runtime: N/A; Memory: N/A]

--- string/remove-k-digits.cpp
+++ string/remove-k-digits.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-      int n = num.size();
-      if(k>n) return "0";
-     while(k>=0){
-        num.erase(0,1);
-        k--;
-     } 
-     if(num[0] == '0'){
-        int i =0;
-        while(num[i]== '0' && i<num.size()){
-            num.erase(0,1);
-            i++;
+    string ans = "";
+
+    void solve(string &num, int i, int k, string curr) {
+        // processed all digits
+        if (i == num.size()) {
+            if (k == 0) {
+                // remove leading zeroes
+                int j = 0;
+                while (j < curr.size() && curr[j] == '0')
+                    j++;
+
+                string temp = curr.substr(j);
+
+                if (temp == "")
+                    temp = "0";
+
+                if (ans == "" || temp.size() < ans.size() ||
+                    (temp.size() == ans.size() && temp < ans)) {
+                    ans = temp;
+                }
+            }
+            return;
         }
-        return num;
-     }
-     if(num.empty()) return "0";
-     return num;
+
+        // Choice 1: keep current digit
+        solve(num, i + 1, k, curr + num[i]);
+
+        // Choice 2: remove current digit
+        if (k > 0) {
+            solve(num, i + 1, k - 1, curr);
+        }
+    }
+
+    string removeKdigits(string num, int k) {
+        if (k == num.size())
+            return "0";
+
+        solve(num, 0, k, "");
+
+        return ans;
     }
 };
