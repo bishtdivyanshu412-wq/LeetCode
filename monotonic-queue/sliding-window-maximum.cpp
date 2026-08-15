514ea4407f23cdb973801e1f00ee4f5d08180fb4
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-15 12:35:04 +0530

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- monotonic-queue/sliding-window-maximum.cpp
+++ monotonic-queue/sliding-window-maximum.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-      int n = nums.size();
-      vector<int>ans;
-      for(int i =0;i<=(n-k);i++){
-        int maxi = nums[i];
-        for(int j = i;j<=(i+k-1);j++){
-            if(nums[j]>maxi) maxi = nums[j];
+        int n = nums.size();
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int i = 0; i < n; i++) {
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
-        ans.push_back(maxi);
-      }  
-      return ans;
+
+        return ans;
     }
 };
