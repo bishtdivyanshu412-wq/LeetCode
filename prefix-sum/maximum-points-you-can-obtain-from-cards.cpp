2e0543282c1b1366daad5af35ef0ae2502a29743
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-15 21:45:12 +0530

    LC-maximum-points-you-can-obtain-from-cards [Runtime: N/A; Memory: N/A]

--- prefix-sum/maximum-points-you-can-obtain-from-cards.cpp
+++ prefix-sum/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int maxScore(vector<int>& c, int k) {
-       int n =  c.size();
-       int sum1 = 0;
-       int sum2 = 0;
-    
-       
-       for(int i =0;i<k;i++){
-        sum1 += c[i];
-        sum2 += c[n-1-i];
-       }
+        int n = c.size();
 
-       return max(sum1,sum2);
+        int lsum = 0;
+        int rsum = 0;
+        int maxsum = 0;
+
+        for(int i =0;i<k;i++){
+            lsum += c[i];
+        }
+        maxsum = lsum;
+
+        for(int i = k-1;i>=0;i--){
+            lsum -= c[i];
+            rsum += c[n-1-i];
+            maxsum = max(maxsum,lsum+rsum);
+        }
+        return maxsum;
     }
-
 };
