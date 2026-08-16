fee6365f219ab0d40bb56f0cbf71d29f29c17867
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-16 11:09:21 +0530

    LC-max-consecutive-ones-iii [Runtime: N/A; Memory: N/A]

--- prefix-sum/max-consecutive-ones-iii.cpp
+++ prefix-sum/max-consecutive-ones-iii.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-      int n = nums.size();
-      int maxcount = 0;
+        int n = nums.size();
+        int maxlen = 0;
 
-      for(int i = 0;i<n;i++){
-        int x = k;
-        int count = 0;
-        for(int j =i;j<n;j++){
-            if(nums[j] == 1){
-                count++;
+        int i = 0;
+        int j = 0;
+
+        while (j < n) {
+
+            if (nums[j] == 0) {
+                k--;
             }
-            else if(nums[j] == 0  && x >0){
-                count++;
-                x--;
+
+            while (k < 0) {
+                if (nums[i] == 0) {
+                    k++;
+                }
+                i++;
             }
-            else if(nums[j] == 0) break;
+
+            maxlen = max(maxlen, j - i + 1);
+
+            j++;
         }
-          maxcount = max(maxcount,count);
-      } 
-      return maxcount; 
+
+        return maxlen;
     }
 };
