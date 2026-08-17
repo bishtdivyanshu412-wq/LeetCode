3ccdd1ef5ffccdfab6d418a831364a85a1e28160
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-17 21:56:45 +0530

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

--- hash-table/number-of-substrings-containing-all-three-characters.cpp
+++ hash-table/number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int i=0,j=0,ans=0;
-        int n=s.size();
-        unordered_map<char,int>mp;
-        while(j<s.size()){
-            mp[s[j]]++;
-            while(mp.size()==3){
-                ans+=(n-j);
-                mp[s[i]]--;
-                if(mp[s[i]]==0)mp.erase(s[i]);
-                i++;
+        int n = s.size();
+
+        vector<int>arr = {-1,-1,-1};
+
+        int cnt = 0;
+
+        for(int i =0;i<n;i++){
+            arr[s[i]-'a'] = i;
+
+            if(arr[0] != -1 && arr[1] != -1 && arr[2] != -1){
+                cnt = cnt +(1 + min(arr[0],min(arr[1],arr[2])));
             }
-            j++;
         }
-        return ans;
+        return cnt;
     }
 };
