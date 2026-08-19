225edee137f186da37959e9fc8dd2afa7230258a
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-19 23:56:10 +0530

    LC-binary-subarrays-with-sum [Runtime: N/A; Memory: N/A]

--- hash-table/binary-subarrays-with-sum.cpp
+++ hash-table/binary-subarrays-with-sum.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+
+    int check(int n , vector<int>& nums , int goal){
+        if(goal<0) return 0;
+
+        int cnt = 0;
+
+        int l = 0 ;
+        int r = 0;
+        int sum = 0;
+
+        while(r<n){
+            sum += nums[r];
+
+            while(sum>goal){
+                sum -= nums[l];
+                l++;
+            }
+            cnt = cnt + (r-l+1);
+            r++;
+        }
+        return cnt;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
         int n = nums.size();
-        int count = 0;
-
-        int sum = 0;
-        map<int,int>mpp;
-        mpp[0] = 1;
-        for(int i =0;i<n;i++){
-         sum+=nums[i];
-         int rem = sum-goal;
-        
-         if(mpp.find(rem)!=mpp.end()){
-            count += mpp[rem];
-         }
-         mpp[sum]++;
-        }
-        return count;
+        int left = check(n,nums,goal);
+        int right = check(n,nums,goal-1);
+        return left-right;
     }
 };
