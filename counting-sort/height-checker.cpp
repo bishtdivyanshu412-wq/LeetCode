9be8f7fce9702aad7b8ba9a35ba075165f484faa
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-19 23:03:32 +0530

    LC-height-checker [Runtime: N/A; Memory: N/A]

---
+++ counting-sort/height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+       int n = heights.size();
+
+       vector<int>ans = heights;
+
+       sort(ans.begin(),ans.end());
+
+       int count = 0;
+
+       for(int i =0;i<n;i++){
+        if(ans[i]!=heights[i]) count++;
+       } 
+
+       return count;
+    }
+};
