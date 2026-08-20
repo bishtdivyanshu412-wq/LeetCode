f506f78e3e884bbb406be1103e166f8ecfb64415
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-20 23:53:20 +0530

    LC-count-number-of-nice-subarrays [Runtime: N/A; Memory: N/A]

--- hash-table/count-number-of-nice-subarrays.cpp
+++ hash-table/count-number-of-nice-subarrays.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
+    int check(int n , int k , vector<int>& nums){
+        if(k<0) return 0;
+        int count = 0;
+
+        int i =0;
+        int j = 0;
+        int sum = 0;
+        while(j<n){
+            sum = sum + (nums[j]%2);
+            while(sum>k){
+                sum  = sum - (nums[i]%2);
+                i++;
+            }
+
+            count += j-i+1;
+            j++;
+        }
+        return count;
+    }
     int numberOfSubarrays(vector<int>& nums, int k) {
       int n = nums.size();
-      int count = 0;
-
-      for(int i =0;i<n;i++){
-        int c= 0;
-        for(int j = i;j<n;j++){
-             if(nums[j]%2 != 0) c++;
-            if(c>k) break;
-            if(c == k) count++;
-        }
-        
-      }  
-      return count ;
+      int left = check(n,k,nums);
+      int right = check(n,k-1,nums);
+      return (left-right);
     }
 };
