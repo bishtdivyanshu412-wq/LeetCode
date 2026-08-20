36d834a4a5f50b200a86e258b21f954e97c38933
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-21 04:48:12 +0530

    LC-lemonade-change [Runtime: N/A; Memory: N/A]

--- greedy/lemonade-change.cpp
+++ greedy/lemonade-change.cpp
@@ -7,25 +7,23 @@ public:
       int c10 = 0;
 
       for(int i =0;i<n;i++){
-        if(bills[i] == 20){
-            if(c5>=3) c5-=3;
-            else if(c5 >=1 && c10>=1 ){
-                c5--;
-                c10--;
-            }
-            else return false;
-        }
+        if(bills[i] == 5) c5++;
         else if(bills[i] == 10){
-            if(c5 >= 1){
+            if(c5>=1){
                 c5--;
                 c10++;
             }
-            else return false;
+            else false;
         }
         else{
-            c5++;
+            if(c5>=1 && c10>=1) {
+                c5--;
+                c10--;
+            }
+            else if(c5>=3) c5 -=3;
+            else return false;
         }
-      }  
+      }
       return true;
     }
 };
