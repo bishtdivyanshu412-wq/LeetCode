f41d4a3d33855c3a4da13f6ce26d277af5f3710e
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-21 19:29:27 +0530

    LC-non-overlapping-intervals [Runtime: N/A; Memory: N/A]

--- sorting/non-overlapping-intervals.cpp
+++ sorting/non-overlapping-intervals.cpp
@@ -3,21 +3,21 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(),nums.end());
-
+        sort(nums.begin(), nums.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+        });
         vector<int>ans;
 
-        ans.push_back(nums[0][0]);
-        ans.push_back(nums[0][1]);
+        int cnt = 1;
 
-        for(int i =1 ;i<n;i++){
-            if(ans.back() == nums[i][0] || ans.back() < nums[i][0]){
-                ans.push_back(nums[i][0]);
-                ans.push_back(nums[i][1]);
-            }
-        }
+        int last = nums[0][1];
         
-        int m = ans.size()/2;
-        return n-m;
+        for(int i =1 ;i<n;i++){
+            if(last<=nums[i][0]){
+                cnt = cnt+1;
+                last = nums[i][1];
+            }
+        }  
+        return n-cnt;
     }
 };
