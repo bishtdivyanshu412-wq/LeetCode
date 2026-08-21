d0a554e92bb1adde5431366c2f611639dd9b60a2
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-21 11:43:24 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- greedy/jump-game-ii.cpp
+++ greedy/jump-game-ii.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return 0;
-        int step = 0;
-        int prevmax = nums[0];
 
-        for(int i =0;i<n;i++){
-            prevmax = max(prevmax,nums[i]);
-            if(prevmax+i >= n-1){
-                step++;
-                return step;
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
             }
-
-            prevmax--;
-            step++;
         }
-        return -1;
+
+        return jumps;
     }
 };
