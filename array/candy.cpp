67c913eeaa15b8792edf67012d81f8ad5e8091a9
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-22 13:31:18 +0530

    LC-candy [Runtime: N/A; Memory: N/A]

--- array/candy.cpp
+++ array/candy.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n =  ratings.size();
+    int candy(vector<int>& nums) {
+        int n = nums.size();
 
         int count = 0;
-        unordered_map<int,int>mpp;
-        for(int i = 0 ;i<n;i++){
-           if(i == 0 ){
-            if(ratings[0]>ratings[1]){
-                mpp[i] = 2;
-            }
-            else mpp[i] = 1;
-           }
-           else if(i == n-1){
-            if(ratings[n-1]>ratings[n-2]){
-                mpp[n-1] = mpp[n-2]+1;
-            }
-            else mpp[n-1] = 1;
-           }
-           else{
-            if(ratings[i]>ratings[i+1] || ratings[i] > ratings[i-1]){
-                mpp[i] = mpp[i-1]+1;
-            }
-            else mpp[i] = 1;
-           }
-        }
-        for(auto it : mpp){
-            count += it.second;
-        }
+        int prev = 0;
 
+        for(int i =0;i<n;i++){
+            if(i==0){
+               if(nums[0]>nums[1]) {
+                count += 2;
+                prev = count;
+               }
+               else{
+                count +=1;
+                prev = count;
+               }
+            }
+            else {
+                if(nums[i]>nums[i-1]){
+                    count+= prev+1;
+                    prev = prev+1;
+                }
+                else{
+                    count+=1;
+                    prev=1;
+                }
+            }
+        }
         return count;
     }
 };
