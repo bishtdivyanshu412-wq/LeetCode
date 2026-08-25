a5a4442f12def70b808208e705d334e0b1402e6b
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-25 20:31:16 +0530

    LC-count-hills-and-valleys-in-an-array [Runtime: N/A; Memory: N/A]

--- array/count-hills-and-valleys-in-an-array.cpp
+++ array/count-hills-and-valleys-in-an-array.cpp
@@ -4,32 +4,13 @@ public:
      int n = nums.size();
 
      int count = 0;
-
+     int prev = nums[0];
      for(int i =1;i<n-1;i++){
-        if(nums[i] == nums[i-1]) continue;
-        int left = -1;
-        int right = -1;
-
-        for(int j = i+1;j<n;j++){
-            if(nums[i]!=nums[j]){
-                right = nums[j];
-                break;
-            }
-        }
-
-        for(int k = i-1;k>=0;k--){
-            if(nums[i]!=nums[k]){
-                left = nums[k];
-                break;
-            }
-        }
-
-        if(left != -1 && right != -1){
-            if(nums[i]>left && nums[i]>right) count++;
-            else if(nums[i]<left && nums[i]<right) count++;
-        }
-     }   
-
+       if(nums[i] == nums[i+1]) continue;
+       if(prev > nums[i] && nums[i]<nums[i+1]) count++;
+       else if(prev<nums[i] && nums[i]>nums[i+1]) count++;
+       prev = nums[i];
+     }
      return count;
     }
 };
