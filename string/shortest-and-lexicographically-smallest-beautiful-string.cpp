b5f021f3219c64576dd95582e0b44f291d48a812
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-26 10:33:59 +0530

    LC-shortest-and-lexicographically-smallest-beautiful-string [Runtime: N/A; Memory: N/A]

--- string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -3,35 +3,38 @@ public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
 
-        string ans = "";
-
-        int start = -1;
-        int end = -1;
-
-        int maxlen = INT_MAX;
-
-        int i =0;
-        int j =0;
+        int i = 0;
         int count = 0;
-        while(j<n){
-            if(s[j] == '1') count++;
-            while(count>=k){
-                if(maxlen >= (j-i+1)){
-                    maxlen = j-i+1;
-                    start = i;
-                    end = j;
+        int bestLen = INT_MAX;
+        int bestStart = -1;
+
+        for (int j = 0; j < n; j++) {
+
+            if (s[j] == '1')
+                count++;
+
+            while (count >= k) {
+
+                int len = j - i + 1;
+
+                if (len < bestLen ||
+                    (len == bestLen &&
+                     (bestStart == -1 || s.compare(i, len, s, bestStart, bestLen) < 0))) {
+
+                    bestLen = len;
+                    bestStart = i;
                 }
 
-                if(s[i] == '1') count--;
-                i++; 
+                if (s[i] == '1')
+                    count--;
+
+                i++;
             }
-            j++;
-        }
-        if(start == -1 && end == -1) return "";
-        for(int i = start ; i<=end;i++){
-            ans += s[i];
         }
 
-        return ans;
+        if (bestStart == -1)
+            return "";
+
+        return s.substr(bestStart, bestLen);
     }
 };
