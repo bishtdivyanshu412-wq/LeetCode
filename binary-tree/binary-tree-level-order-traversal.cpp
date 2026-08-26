cd96ef2a44895ade816b4e3698e7508f2c4cb2a6
Divyanshu Bisht <bishtdivyanshu412@gmail.com>
2026-08-26 22:13:43 +0530

    LC-binary-tree-level-order-traversal [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/binary-tree-level-order-traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+
+        if(root == NULL) return result;
+
+        queue<TreeNode*>q;
+
+        q.push(root);
+
+        while(!q.empty()){
+            int n = q.size();
+            vector<int>ans;
+
+            for(int i =0;i<n;i++){
+                TreeNode* newnode = q.front();
+                q.pop();
+                if(newnode->left != NULL) q.push(newnode->left);
+                if(newnode->right != NULL) q.push(newnode->right);
+                ans.push_back(newnode->val);
+            }
+            result.push_back(ans);
+        }
+
+        return result;
+    }
+};
